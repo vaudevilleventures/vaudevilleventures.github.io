1656bf5069379673f6a91ccf769449d21a07b808
jernaks <jernaks@gmail.com>
2018-04-16 16:56:14 -0400

    New changes
    Amendments

---
+++ Mail Signatures/<style type="text:css">.js
@@ -0,0 +1,154 @@
+<style type="text/css">
+
+</style>
+<div id="signature-preview-wrapper" class="col-sm-8">
+
+
+<signature-preview ng-show="isPreviewVisible()"><div class="panel panel-
+info">
+
+<div id="signature-preview-workspace" class="panel-body">
+
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" class="ng-scope">
+<!--UNCOMMENT</HEAD>
+<BODY style="font-family:prompt, prompt, sans-serif;">UNCOMMENT-->
+<table style="font-family:gadget, sans-serif;" cellpadding="0" cellspacing="0" class="ng-scope">
+<tbody>
+<tr>
+
+
+<td style="font-family:prompt; border-right:2px solid #BD272D; padding-right:6px; text-align: right; vertical-align:top; "
+valign="top"; >
+
+<table style="font-family:prompt; margin-left:auto" cellpadding="0" cellspacing="0">
+
+<tbody>
+<tr>
+<td style="padding: 3px; font-family:gadget; vertical-align:bottom; text-align:left;" valign="bottom"; align="left" >
+<span style="font-family:gadget; font-size:12pt; font-weight:bold"; class="ng-binding">Jane Doe<!-- ngIf:
+showField('title') --><span ng-if="showField('title')" class="ng-scope"><br></span>
+
+<!-- end ngIf: showField('title') --></span>
+
+<!-- ngIf: showField('title') --> <style="padding: 3px; font-family:prompt; font-size:10pt;" ng-if="showField('title')"
+class="ng-binding ng-scope"> Position </span>
+
+
+<span style="font-family:gadget; font-size:10pt; " ng-if="showField('title')" class="ng-binding ng-
+scope"> | </span>
+
+
+
+<span style= "font-family:gadget; font-size:10pt; font-style: italic" ng-if="showField('title')" class= "ng-
+binding ng-scope"> Department </span>
+
+
+
+<!-- end ngIf: showField('title') -->
+
+</td>
+</tr>
+<!-- ngIf: isAnyFieldEntered('phone, mobile, address1, address2') --><tr ng-if="isAnyFieldEntered('phone, mobile,
+address1, address2')" class="ng-scope">
+
+<td style="font-family:prompt; height:6px; vertical-align:top; padding-left:3px; ;" valign="top" align="top">
+
+<!-- ngIf: showField('phone') --><span style="font-family:gadget; font-size:10pt;" ng-if="showField('phone')"
+class="ng-binding ng-scope"> # : XXX XXX XXXX<!-- ngIf: showField('mobile') --> <span ng-if="showField('mobile')"
+class="ng-scope"><br></span><!-- end ngIf: showField('mobile') --></span><!-- end ngIf: showField('phone') -->
+
+
+</td>
+</tr><!-- end ngIf: isAnyFieldEntered('phone, mobile, address1, address2') -->
+<!-- ngIf: isAnyFieldEntered('facebookURL, twitterURL, youtubeURL, linkedinURL, gplusURL, instagramURL,
+website') --><tr ng-if="isAnyFieldEntered('facebookURL, twitterURL, youtubeURL, linkedinURL, gplusURL,
+instagramURL, website')" class="ng-scope">
+
+<td align="left" valign="middle" class="lastbox" style="font-family:gadget; height:10px; vertical-align:middle; text-
+align:left;">
+
+
+<!-- ngIf: showField('facebookURL') --> <span ng-if="showField('facebookURL')" class="ng-scope"><a
+
+href="https://www.facebook.com/vaudventures/" target = "_blank"><img border="0" width="10" ng-
+src="https://signature-generator.cdn.codetwo.com/images/logo1/fb.png" alt="Facebook icon" style= "padding: 3px;
+
+border:0; height:20px; width:auto" src="https://en.facebookbrand.com/wp-content/uploads/2016/05/FB-fLogo-Blue-
+broadcast-2.png"></a></span><!-- end ngIf: showField('facebookURL') -->
+
+
+<!-- ngIf: showField('linkedinURL') -->
+<span ng-if="showField('linkedinURL')" class="ng-scope"><a
+
+href="https://www.linkedin.com/company/vaudeville-ventures" target = "_blank"><img border="0" width="20px"
+alt="linkedin icon" style="padding: 3px; border:0; height:20px; width:auto" src=
+"http://www.freepngimg.com/thumb/linkedin/3-2-linkedin-download-png-thumb.png"></a></span>
+<!-- end ngIf: showField('linkedinURL') -->
+
+<!-- ngIf: showField('twitterURL') -->
+<span ng-if="showField('twitterURL')" class="ng-scope"><a href= "https://twitter.com/VVIdeas" target
+= "_blank"><img border="0" width="20px" ng-src= "" alt="Facebook icon" style="padding:3px; border:0; height:20px;
+width:auto" src= "https://avatars1.githubusercontent.com/u/50278?s=400&v=4"></a></span>
+
+<!-- end ngIf: showField('twitterURL') -->
+
+<!-- ngIf: showField('instagramURL') -->
+
+</td>
+</tr><!-- end ngIf: isAnyFieldEntered('facebookURL, twitterURL, youtubeURL, linkedinURL, gplusURL,
+instagramURL, website') -->
+
+</tbody>
+</table>
+<td style= "padding-left:3px; vertical-align:top; font-family:prompt;">
+<table style="font-family: prompt;" cellpadding="0px" cellspacing="0">
+<tbody>
+<tr>
+<td style="height:55px; font-family:prompt; vertical-align:left;" valign="left">
+<!-- ngIf: showField('logoURL') && showField('logo') --><a ng-if="showField('logoURL') &amp;&amp;
+showField('logo')" href="https://www.vaudevilleventures.com/" target="_blank" class="ng-scope">
+
+
+<img ng-
+src="http://static1.squarespace.com/static/56702c2169a91aeaac4f1875/t/5776ca499de4bb956cf7bf84/1510097851100/?
+
+format=1500w" alt="Logo" style="padding-left:3px; height:50px; width:auto; border:0; margin-top:10px; height= 10px
+border= 0"
+
+src=
+"http://static1.squarespace.com/static/56702c2169a91aeaac4f1875/t/5776ca499de4bb956cf7bf84/1510097851100/?
+format=1500w" ></a><!-- end ngIf: showField('logoURL') && showField('logo') -->
+<!-- ngIf: !showField('logoURL') && showField('logo') -->
+</td>
+</tr>
+<!-- ngIf: isAnyFieldEntered('address1, address2, phone, mobile' ) --><tr ng-if="isAnyFieldEntered('address1,
+address2, phone, mobile' )" class="ng-scope"> </tr><!-- end ngIf: isAnyFieldEntered('address1, address2, phone,
+mobile' ) -->
+<!-- ngIf: isAnyFieldEntered('facebookURL, twitterURL, youtubeURL, linkedinURL, gplusURL, instagramURL,
+website') --><tr ng-if="isAnyFieldEntered('facebookURL, twitterURL, youtubeURL, linkedinURL, gplusURL,
+instagramURL, website')" class="ng-scope"> </tr><!-- end ngIf: isAnyFieldEntered('facebookURL, twitterURL,
+youtubeURL, linkedinURL, gplusURL, instagramURL, website') -->
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<!--UNCOMMENT
+</BODY>
+</HTML>
+
+UNCOMMENT--></ng-include>
+</div>
+</div>
+</signature-preview>
+
+<signature-deployment ng-show="!isPreviewVisible()" selected-
+template="selectedTemplate" selected-platform-path="selectedPlatformPath" class="ng-isolate-scope ng-hide"><div
+
+id="application-steps">
+<!-- ngInclude: --><ng-include src="$ctrl.selectedPlatformPath" class="ng-scope"> </ng-include>
+
+</div>
+</signature-deployment>
+</div>
